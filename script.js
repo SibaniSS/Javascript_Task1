fb027106d54b07626b57297251495dadda1bf2eb
SibaniSS <143472140+SibaniSS@users.noreply.github.com>
2024-06-30 17:03:28 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+let boxes = document.querySelectorAll(".box");
+let turn = "X";
+let isGameOver = false;
+let turnIndicator = document.querySelector("#turn-indicator");
+
+boxes.forEach(e => {
+    e.innerHTML = "";
+    e.addEventListener("click", () => {
+        if (!isGameOver && e.innerHTML === "") {
+            e.innerHTML = turn;
+            cheakWin();
+            cheakDraw();
+            changeTurn();
+        }
+    });
+});
+
+function changeTurn() {
+    turn = turn === "X" ? "O" : "X";
+    turnIndicator.innerHTML = "Turn: " + turn;
+}
+
+function cheakWin() {
+    let winConditions = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]
+    ];
+    for (let i = 0; i < winConditions.length; i++) {
+        let v0 = boxes[winConditions[i][0]].innerHTML;
+        let v1 = boxes[winConditions[i][1]].innerHTML;
+        let v2 = boxes[winConditions[i][2]].innerHTML;
+
+        if (v0 !== "" && v0 === v1 && v0 === v2) {
+            isGameOver = true;
+            document.querySelector("#results").innerHTML = turn + " wins";
+            document.querySelector("#play-again").style.display = "inline";
+
+            for (let j = 0; j < 3; j++) {
+                boxes[winConditions[i][j]].style.backgroundColor = "#08D9D6"; //keeps the winning condition to be blue
+                boxes[winConditions[i][j]].style.color = "#000"; // changes the text color to black
+            }
+        }
+    }
+}
+
+function cheakDraw() {
+    if (!isGameOver) {
+        let isDraw = true;
+        boxes.forEach(e => {
+            if (e.innerHTML === "") isDraw = false;
+        });
+
+        if (isDraw) {
+            isGameOver = true;
+            document.querySelector("#results").innerHTML = "Draw";
+            document.querySelector("#play-again").style.display = "inline";
+        }
+    }
+}
+
+document.querySelector("#play-again").addEventListener("click", () => {
+    isGameOver = false;
+    turn = "X";
+    turnIndicator.innerHTML = "Turn: " + turn;
+    document.querySelector("#results").innerHTML = "";
+    document.querySelector("#play-again").style.display = "none";
+
+    boxes.forEach(e => {
+        e.innerHTML = "";
+        e.style.removeProperty("background-color");
+        e.style.color = "#fff";
+    });
+});
